2c92169e13084ab54bde77876ccb94e430829f3b
Jerrin Eldo Mazhuvancherry P <jerin.jollybhavan@gmail.com>
2021-05-31 01:21:26 -0400

    Day 3 and Day 4

--- readme.md
+++ readme.md
@@ -6,3 +6,12 @@
 - was able to understand the basics of JSX.
 
 ![Image - Day 1 & Day 2](./images/Day1.png)
+
+## Day 3 & Day 4
+
+- was able to understand how the special children props is used to wrap components with custom wrappers. The value of the special children prop would be the content of the opening and closing tag of your custom component.
+- Composition is a manner in which we combine components together to form the overall UI. Here we use simple Html elements to form a custom html element which is basically used by another component to form the overall UI.
+- Each component can only return one root element , which wraps all the other children elements.
+- The concept of USESATE - This concept allows us to define values as state where changes to these values ultimately gets reflected in the component function being called again. It is a react hook and can be called only inside a component. in simple words , useState hooks allows to change the state of a component. In react , one component is called once . only if the state of the react component is changed through the useState hook , the component is reevaluated and the state would be changed.
+
+![Image - Day 3 & Day 4](./images/Day4.png)
--- src/App.js
+++ src/App.js
@@ -1,86 +1,15 @@
 import React, { Component } from "react"; //required for rendering to the DOM
 import "./App.css";
-//import Person from './Person/Person';
 
-// class App extends Component {
-//   state = {
-//     persons: [
-//       { name: 'Max', age: 28 },
-//       { name: 'Manu', age: 29 },
-//       { name: 'Stephanie', age: 26 }
-//     ],
-//     otherstate: 'some other value'
-//   }
+import Expenses from "./components/Expenses/Expenses";
 
-//   switchNameHandler = (newName) => {
-//     //console.log('Was clicked');
-//     //DONT DO THIS : this.state.person[0].name = 'Maxmilian';
-//     this.setState({
-//       persons: [
-//         { name: newName, age: 28 },
-//         { name: 'Manu', age: 29 },
-//         { name: 'Stephanie', age: 27 }
-//       ]
-//     })
-//   }
+// function App() {
+// const para = document.createElement('p');
+// para.textContent = 'This is also visible';
+// document.getElementsById('root').append(para);
+// This is cumbersome, when its a very big application
 
-//   nameChangedHandler = (event) => {
-//     this.setState( {
-//       persons: [
-//         { name: 'Max', age: 28 },
-//         { name: event.target.value, age: 29 },
-//         { name: 'Stephanie', age: 26 }
-//       ]
-//     } )
-//   }
-//   render() {
-
-//     const style = {
-//       backgroundColor : 'white',
-//       font:'inherit',
-//       border: '1px solid blue',
-//       padding: '8px',
-//       cursor: 'pointer'
-//     };
-
-//     return (
-//       <div className="App">
-//         <h1>Jerrin here</h1>
-//         <p>This is really working </p>
-//         <button
-//           style={style}
-//           onClick = {this.switchNameHandler.bind(this,'Maximillian')}>Switch Name</button>
-//         <Person
-//           name = {this.state.persons[0].name}
-//           age = {this.state.persons[0].age}/>
-//         <Person
-//           name = {this.state.persons[1].name}
-//           age = {this.state.persons[1].age}
-//           click = {this.switchNameHandler.bind(this,'Max!')}
-//           changed = {this.nameChangedHandler }>My hobbies: Racing
-//         </Person>
-//         <Person
-//           name = {this.state.persons[2].name}
-//           age = {this.state.persons[2].age} />
-//       </div>
-//     );
-//     // Writing code in React and not in JSx Like above
-//     // return React.createElement('div',null,'h1','Hi I am Jerrin');
-//     // Below code is the compiled version of the JSx Code above written in JSx
-//     //return React.createElement('div', {className:'App'} ,React.createElement('h1',null,'Does this work now?'));
-//   }
-// }
-
-// export default App;
-
-import Expenses from "./components/Expenses";
-
-function App() {
-  // const para = document.createElement('p');
-  // para.textContent = 'This is also visible';
-  // document.getElementsById('root').append(para);
-  //This is cumbersome, when its a very big application
-
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -110,6 +39,6 @@ function App() {
     </div>
     //here above we just define what the desired end result is
   );
-}
+};
 
 export default App;
